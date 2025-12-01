8174fa5353fe31d353a2a774c39edb46c07a5451
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-12-01 15:11:16 +0300

    feat: update the font in my bookings

--- src/app/my-bookings/page.js
+++ src/app/my-bookings/page.js
@@ -35,7 +35,7 @@ export default function MyBookingsPage() {
               <h2 className="text-xl font-semibold mt-3">{booking.name}</h2>
               <p className="text-gray-700 font-medium">$ {booking.price}</p>
 
-              <div className="text-sm text-gray-600 mt-2">
+              <div className="text-base text-gray-600 mt-2">
                 <p><strong>Pickup:</strong> {booking.pickupDate}</p>
                 <p><strong>Return:</strong> {booking.returnDate}</p>
                 <p><strong>Location:</strong> {booking.location}</p>
