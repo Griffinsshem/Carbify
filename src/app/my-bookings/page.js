4682e2ac1ab700e285a6375c44437ecf5e662e4a
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-12-02 08:18:56 +0300

    fix: the import path for firebase auth

--- src/app/my-bookings/page.js
+++ src/app/my-bookings/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import { auth } from "@/firebase";
+import { auth } from "@/firebase/config";
 
 // Lucide Icons
 import {
